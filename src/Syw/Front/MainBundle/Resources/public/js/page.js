ae331a2cfa30c7678631cb07869e69b3a98f196d
Christin Löhner <c.loehner@akademie-sport-gesundheit.de>
2018-04-27 06:04:17 +0200

    ifix

--- src/Syw/Front/MainBundle/Resources/public/js/page.js
+++ src/Syw/Front/MainBundle/Resources/public/js/page.js
@@ -1,12 +1,4 @@
 
-var $loading = $('#loadingDiv').hide();
-$(document)
-    .ajaxStart(function () {
-        $loading.show();
-    })
-    .ajaxStop(function () {
-        $loading.hide();
-    });
 
 $(document).ready(function() {
 
